c475aef74acec93b74210a00a1821fe575e92f3f
Al <al.manja@gmx.com>
2017-01-27 12:01:03 -0700

    Update logs.py

--- logs.py
+++ logs.py
@@ -3,61 +3,75 @@ from os.path import expanduser
 """
 This script prints out some errors from log files (Xorg.0.log, Xorg.1.log, pacman.log and journalctl.txt
 It is intended for Linux OS (Arch based: Manjaro) For other Linux versions comment out function: read_pacman()
-Before you use the script, in terminal run: journalctl -b > journalctl.txt to pipe binary log to txt
-Alternatively you can define path, run: journalctl -b > /path/to/your/home/journalctl.txt
+Before you run the script, in terminal run: journalctl > journalctl.txt to pipe binary log to txt
+Alternative, run: journalctl -b > /path/to/your/home/journalctl.txt
 """
+
+# FileNotFoundError:
 
 home = expanduser("~")
 
 
 def read_xorg0():
     """from Xorg.0.log print lines that contain words: failed, error, (WW)"""
-    with open('/var/log/Xorg.0.log', 'r') as f:
-        print('==============')
-        print('| Xorg.0.log |')
-        print('==============')
-        for line in f:
-            if 'failed' in line or 'error' in line or '(WW)' in line:
-                print(line, end='')
-    print()
+    try:
+        with open('/var/log/Xorg.0.log', 'r') as f:
+            print('==============')
+            print('| Xorg.0.log |')
+            print('==============')
+            for line in f:
+                if 'failed' in line or 'error' in line or '(WW)' in line:
+                    print(line, end='')
+        print()
+    except:
+        print('Missing file: Xorg.0.log')
 
 
 def read_xorg1():
     """from Xorg.1.log print lines that contain words: failed, error, (WW)"""
-    with open('/var/log/Xorg.1.log', 'r') as f:
-        print('==============')
-        print('| Xorg.1.log |')
-        print('==============')
-        for line in f:
-            if 'failed' in line or 'error' in line or '(WW)' in line:
-                print(line, end='')
-    print()
+    try:
+        with open('/var/log/Xorg.1.log', 'r') as f:
+            print('==============')
+            print('| Xorg.1.log |')
+            print('==============')
+            for line in f:
+                if 'failed' in line or 'error' in line or '(WW)' in line:
+                    print(line, end='')
+        print()
+    except:
+        print('Missing file: Xorg.1.log')
 
 
 def read_pacman():
     """from pacman.log print lines that contain words: pacsave, pacnew, pacorig, warning"""
-    with open('/var/log/pacman.log', 'r') as f:
-        print('==============')
-        print('| pacman.log |')
-        print('==============')
-        for line in f:
-            if 'pacsave' in line or 'pacnew' in line or 'pacorig' in line or 'warning' in line:
-                print(line, end='')
-    print()
+    try:
+        with open('/var/log/pacman.log', 'r') as f:
+            print('==============')
+            print('| pacman.log |')
+            print('==============')
+            for line in f:
+                if 'pacsave' in line or 'pacnew' in line or 'pacorig' in line or 'warning' in line:
+                    print(line, end='')
+        print()
+    except:
+        print('Missing file: pacman.log   This is not Arch based distribution?')
 
 
 def read_journalctl():
     """from journalctl.txt print lines that contain: emergency, alert, critical & failed; 0: emerg, 1: alert 2: crit"""
-    with open(home + '/journalctl.txt', 'r') as f:
-        print('==============')
-        print('| journalctl |')
-        print('==============')
-        key_word = ['emergency', 'Emergency', 'EMERGENCY', 'alert', 'Alert', 'ALERT', 'critical', 'Critical',
-                    'CRITICAL', 'failed']
-        for line in f:
-            for word in key_word:
-                if word in line:
-                    print(line, end='')
+    try:
+        with open(home + '/journalctl.txt', 'r') as f:
+            print('==============')
+            print('| journalctl |')
+            print('==============')
+            key_word = ['emergency', 'Emergency', 'EMERGENCY', 'alert', 'Alert', 'ALERT', 'critical', 'Critical',
+                        'CRITICAL', 'failed']
+            for line in f:
+                for word in key_word:
+                    if word in line:
+                        print(line, end='')
+    except:
+        print('Missing file: journalctl.txt    Did you run (terminal): journalctl > journalctl.txt ?')
 
 read_xorg0()
 read_xorg1()

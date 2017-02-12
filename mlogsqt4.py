497d843492d4d5aa88725e4a46102ccb9d67c58f
nyt <jonsspamaccout@gmail.com>
2017-02-11 16:19:57 -0800

    reduced repeated code

--- mlogsqt4.py
+++ mlogsqt4.py
@@ -9,235 +9,119 @@ from PyQt4.QtCore import Qt          # provides Qt identifiers
 from PyQt4.QtGui import QPushButton, QApplication
 from sh import inxi                  # The sh library is awesome for linux users
 
-chkbut1=0
-chkbut2=0
-chkbut3=0
-chkbut4=0
-chkbut6=0
-chkbut7=0
-chkbut8=0
-chkbut9=0
+TMP_FILE = "/tmp/mlogsout.txt"
 
+HEADER = '''
+=================
+|{:^15}|   {}
+=================
+'''
+
+checkbuttons = [
+    '&Inxi - (inxi -Fxzc0)',
+    '&Xorg.0 - (/var/log/Xorg.0.log)',
+    'X&org.1 - (/var/log/Xorg.1.log)',
+    '&pacman.log - (/var/log/pacman.log)',
+    'journalctl.txt - (&Emergency)',
+    'journalctl.txt - (&Alert)',
+    'journalctl.txt - (&Critical)',
+    'journalctl.txt - (&Failed)',
+]
+
+def look_in_file(file_name, kws):
+    """reads a file and returns only the lines that contain one of the keywords"""
+    with open(file_name) as f:
+        return "".join(filter(lambda line: any(kw in line for kw in kws), f))
 
 class Window(QtGui.QWidget):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
 
-        # creates the checkbox
-        checkbox1 = QtGui.QCheckBox('&Inxi - (inxi -Fxzc0)')                # the shortcut key is ALT + I
-        checkbox2 = QtGui.QCheckBox('&Xorg.0 - (/var/log/Xorg.0.log)')      # the shortcut key is ALT + x
-        checkbox3 = QtGui.QCheckBox('X&org.1 - (/var/log/Xorg.1.log)')
-        checkbox4 = QtGui.QCheckBox('&pacman.log - (/var/log/pacman.log)')
-        checkbox6 = QtGui.QCheckBox('journalctl.txt - (&Emergency)')
-        checkbox7 = QtGui.QCheckBox('journalctl.txt - (&Alert)')
-        checkbox8 = QtGui.QCheckBox('journalctl.txt - (&Critical)')
-        checkbox9 = QtGui.QCheckBox('journalctl.txt - (&Failed)')
-
-        btn = QPushButton("&Show Errors (/tmp/mlogsout.txt))", self)
-        btn2 = QPushButton("&PasteBin - not working yet", self)
-
-        # connects the 'stateChanged()' signal with the 'checkbox_state_changed()' slot
-        checkbox1.stateChanged.connect(self.checkbox1_state_changed)
-        checkbox2.stateChanged.connect(self.checkbox2_state_changed)
-        checkbox3.stateChanged.connect(self.checkbox3_state_changed)
-        checkbox4.stateChanged.connect(self.checkbox4_state_changed)
-        checkbox6.stateChanged.connect(self.checkbox6_state_changed)
-        checkbox7.stateChanged.connect(self.checkbox7_state_changed)
-        checkbox8.stateChanged.connect(self.checkbox8_state_changed)
-        checkbox9.stateChanged.connect(self.checkbox9_state_changed)
-
-        btn.clicked.connect(partial(self.to_computer))
-        btn.clicked.connect(self.to_editor)
-        # btn.clicked.connect(partial(self.to_computer))
-        # btn2.clicked.connect(self.to_web)
-
+        self.checks = [False]*len(checkbuttons) # initialize all buttons to False
 
         # creates a vertical box layout for the window
         vlayout = QtGui.QVBoxLayout()
-        vlayout.addWidget(checkbox1)  # adds the checkbox to the layout
-        vlayout.addWidget(checkbox2)
-        vlayout.addWidget(checkbox3)
-        vlayout.addWidget(checkbox4)
-        vlayout.addWidget(checkbox6)
-        vlayout.addWidget(checkbox7)
-        vlayout.addWidget(checkbox8)
-        vlayout.addWidget(checkbox9)
+        # creates the checkboxes
+        for idx, text in enumerate(checkbuttons):
+            checkbox = QtGui.QCheckBox(text)
+            # connects the 'stateChanged()' signal with the 'checkbox_state_changed()' slot
+            checkbox.stateChanged.connect(partial(self.checkbox_state_changed, idx))
+            vlayout.addWidget(checkbox) # adds the checkbox to the layout
+
+        btn = QPushButton("&Show Errors ({})".format(TMP_FILE), self)
+        btn2 = QPushButton("&PasteBin - not working yet", self)
+
+        btn.clicked.connect(self.to_computer)
+        btn.clicked.connect(self.to_editor)
+
         vlayout.addWidget(btn)
         vlayout.addWidget(btn2)
         vlayout.addStretch()
         self.setLayout(vlayout)  # sets the window layout
 
-    # 'checkbox_state_changed()' slot
-    @pyqtSlot(int)
-    def checkbox1_state_changed(self, state):
-        """Full system and hardware information (Inxi -Fxzc0)"""
-        if state == Qt.Checked:
-            global chkbut1
-            chkbut1=1
-        if state != Qt.Checked:
-            chkbut1=0
-
-    def checkbox2_state_changed(self, state):
-        """from Xorg.0.log print lines that contain words: failed, error, (WW)"""
-        if state == Qt.Checked:
-            global chkbut2
-            chkbut2=1
-        if state != Qt.Checked:
-            chkbut2=0
-
-    def checkbox3_state_changed(self, state):
-        """from Xorg.1.log print lines that contain words: failed, error, (WW)"""
-        if state == Qt.Checked:
-            global chkbut3
-            chkbut3=1
-        if state != Qt.Checked:
-            chkbut3=0
-
-    def checkbox4_state_changed(self, state):
-        """from pacman.log print lines that contain words: pacsave, pacnew, pacorig"""
-        if state == Qt.Checked:
-            global chkbut4
-            chkbut4=1
-        if state != Qt.Checked:
-            chkbut4=0
-
-    def checkbox6_state_changed(self, state):
-        """from journalctl.txt returns lines that contain keyword: emergency"""
-        if state == Qt.Checked:
-            global chkbut6
-            chkbut6 = 1
-        if state != Qt.Checked:
-            chkbut6=0
-
-    def checkbox7_state_changed(self, state):
-        """from journalctl.txt returns lines that contain: alert"""
-        if state == Qt.Checked:
-            global chkbut7
-            chkbut7 = 1
-        if state != Qt.Checked:
-            chkbut7=0
-
-    def checkbox8_state_changed(self, state):
-        """from journalctl.txt returns lines that contain: critical"""
-        if state == Qt.Checked:
-            global chkbut8
-            chkbut8 = 1
-        if state != Qt.Checked:
-            chkbut8=0
-
-    def checkbox9_state_changed(self, state):
-        """from journalctl.txt returns lines that contain: failed"""
-        if state == Qt.Checked:
-            global chkbut9
-            chkbut9 = 1
-        if state != Qt.Checked:
-            chkbut9=0
+    def checkbox_state_changed(self, idx, state):
+        self.checks[idx] = state == Qt.Checked
 
     def to_computer(self, text):
-        f = open('/tmp/mlogsout.txt', 'w')  # write mode clears any previous content from the file if it exists
+        f = open(TMP_FILE, 'w')  # write mode clears any previous content from the file if it exists
 
-        if chkbut1 == 1:
+        if self.checks[0]:
             # write output of 'Inxi -Fxzc0' into /tmp/mlogsout.txt
             print("Saving: inxi to file")
-            f.write('===============\n')
-            f.write('| Inxi -Fxzc0 |   Listing computer information\n')
-            f.write('===============\n')
+            f.write(HEADER.format("Inxi -Fxzc0", "Listing computer information"))
             f.write(str(inxi('-Fxzc0')))
             f.write('\n')
 
-        if chkbut2 == 1:
+        if self.checks[1]:
             print("Saving: Xorg.0.log to file")
-            with open('/var/log/Xorg.0.log', 'r') as f1:
-                f.write('==============\n')
-                f.write('| Xorg.0.log |   (searching for: failed, error & (WW) keywords\n')
-                f.write('==============\n')
-                for line in f1:
-                    if 'failed' in line or 'error' in line or '(WW)' in line:
-                        f.write(line)
+            f.write(HEADER.format("Xorg.0.log", "searching for: failed, error & (WW) keywords"))
+            f.write(look_in_file('/var/log/Xorg.0.log', ['failed', 'error', '(WW)']))
             f.write('\n')
 
-        if chkbut3 == 1:
+        if self.checks[2]:
             print("Saving: Xorg.1.log to file")
-            with open('/var/log/Xorg.1.log', 'r') as f2:
-                f.write('==============\n')
-                f.write('| Xorg.1.log |   (searching for: failed, error & (WW) keywords\n')
-                f.write('==============\n')
-                for line in f2:
-                    if 'failed' in line or 'error' in line or '(WW)' in line:
-                        f.write(line)
+            f.write(HEADER.format("Xorg.1.log", "searching for: failed, error & (WW) keywords"))
+            f.write(look_in_file('/var/log/Xorg.1.log', ['failed', 'error', '(WW)']))
             f.write('\n')
 
-        if chkbut4 == 1:
+        if self.checks[3]:
             print("Saving: pacman.log to file")
-            with open('/var/log/pacman.log', 'r') as f3:
-                f.write('==============\n')
-                f.write('| pacman.log |   (searching for: pacsave, pacnew, pacorig keywords\n')
-                f.write('==============\n')
-                for line in f3:
-                    if 'pacsave' in line or 'pacnew' in line or 'pacorig' in line:
-                        f.write(line)
+            f.write(HEADER.format("pacman.log", "searching for: pacsave, pacnew, pacorig keywords"))
+            f.write(look_in_file('/var/log/pacman.log', ['pacsave', 'pacnew', 'pacorig']))
             f.write('\n')
 
-        if chkbut6 == 1:
+        if self.checks[4]:
             print("Saving: journalctl (mergency) to file")
             os.system("journalctl -b > /tmp/journalctl.txt")
-            with open('/tmp/journalctl.txt', 'r') as f4:
-                f.write('==================\n')
-                f.write('| journalctl.txt |   Searching for: Emergency keywords\n')
-                f.write('==================\n')
-                key_word = ['emergency', 'Emergency', 'EMERGENCY']
-                for line in f4:
-                    for word in key_word:
-                        if word in line:
-                            f.write(line)
+            f.write(HEADER.format("journalctl.txt", "Searching for: Emergency keywords"))
+            f.write(look_in_file('/tmp/journalctl.txt', ['emergency', 'Emergency', 'EMERGENCY']))
             f.write('\n')
 
-        if chkbut7 == 1:
+        if self.checks[5]:
             print("Saving: journalctl (alert) to file")
             os.system("journalctl -b > /tmp/journalctl.txt")
-            with open('/tmp/journalctl.txt', 'r') as f5:
-                f.write('==================\n')
-                f.write('| journalctl.txt |   Searching for: Alert keywords\n')
-                f.write('==================\n')
-                key_word = ['alert', 'Alert', 'ALERT']
-                for line in f5:
-                    for word in key_word:
-                        if word in line:
-                            f.write(line)
+            f.write(HEADER.format("journalctl.txt", "Searching for: Alert keywords"))
+            f.write(look_in_file('/tmp/journalctl.txt', ['alert', 'Alert', 'ALERT']))
             f.write('\n')
 
-        if chkbut8 == 1:
+        if self.checks[6]:
             print("Saving: journalctl (critical) to file")
             os.system("journalctl -b > /tmp/journalctl.txt")
-            with open('/tmp/journalctl.txt', 'r') as f6:
-                f.write('==================\n')
-                f.write('| journalctl.txt |   Searching for: Critical keywords\n')
-                f.write('==================\n')
-                key_word = ['critical', 'Critical', 'CRITICAL']
-                for line in f6:
-                    for word in key_word:
-                        if word in line:
-                            f.write(line)
+            f.write(HEADER.format("journalctl.txt", "Searching for: Critical keywords"))
+            f.write(look_in_file('/tmp/journalctl.txt', ['critical', 'Critical', 'CRITICAL']))
             f.write('\n')
 
-        if chkbut9 == 1:
+        if self.checks[7]:
             print("Saving: journalctl (failed) to file")
             os.system("journalctl -b > /tmp/journalctl.txt")
-            with open('/tmp/journalctl.txt', 'r') as f7:
-                f.write('==================\n')
-                f.write('| journalctl.txt |   Searching for: Failed keywords\n')
-                f.write('==================\n')
-                key_word = ['failed', 'Failed', 'FAILED']
-                for line in f7:
-                    for word in key_word:
-                        if word in line:
-                            f.write(line)
+            f.write(HEADER.format("journalctl.txt", "Searching for: Failed keywords"))
+            f.write(look_in_file('/tmp/journalctl.txt', ['failed', 'Failed', 'FAILED']))
             f.write('\n')
 
         f.close()
 
     def to_editor(self):
-        os.system("xdg-open /tmp/mlogsout.txt")
+        os.system("xdg-open "+TMP_FILE)
 
     def to_web(self):
         pass
